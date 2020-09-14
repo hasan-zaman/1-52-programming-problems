c77f4028292fe32f430b957d86c58033520dadb5
hasan-zaman <hasanuzzaman933@gmail.com>
2020-09-14 22:41:43 +0800

    p-09

---
+++ problem_9.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int T, N, sq_root;
+
+    scanf("%d", &T);
+
+    for(int i=0; i<T; i++){
+
+        scanf("%d", &N);
+        sq_root = sqrt(N);
+
+        if(sq_root * sq_root == N)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+    return 0;
+}
