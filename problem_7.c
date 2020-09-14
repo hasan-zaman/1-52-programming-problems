3d7b5abbd4179c6d3da4a70ff385fbbbccb8d429
hasan-zaman <hasanuzzaman933@gmail.com>
2020-09-14 22:41:33 +0800

    p-07

---
+++ problem_7.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int T, N;
+
+    scanf("%d", &T);
+
+    for(int i=1; i<=T; i++){
+
+        char line[100000];
+        scanf(" %[^\n]", line);
+
+        char *p, *e;
+        long input;
+        int count = 0;
+        p = line;
+
+        for(p = line; ; p = e){
+            input = strtol(p, &e, 10);
+            if(p==e){
+                break;
+            }
+            count++;
+        }
+        printf("%d\n", count);
+    }
+
+    return 0;
+}
