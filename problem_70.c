6d39e80e5b586746ef4135922bcefb7f91452be6
hasan-zaman <hasanuzzaman933@gmail.com>
2020-09-28 12:19:21 +0800

    Bangladesh

---
+++ problem_70.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char country[] = {'I','\n','L','o','v','e','\n','B','a','n','g','l','a','d','e','s','h','.','\0'};
+    //char s1[] = {'I', '\0'};
+    //char s2[] = {'L', 'o', 'v', 'e', '\0'};
+    //char s3[] = {'B', 'a', 'n', 'g', 'l', 'a', 'd', 'e', 's', 'h', '.', '\0'};
+
+
+    printf("%s\n",country);
+    //printf("%s\n",s2);
+   // printf("%s",s3);
+
+    return 0;
+}
