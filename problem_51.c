37a5e2a36fa1d087779a4a81ea4b0354a8c5b034
hasan-zaman <hasanuzzaman933@gmail.com>
2020-09-29 21:54:46 +0800

    khoj The search-01

---
+++ problem_51.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    int T, i, j;
+    char str1[128], str2[128];
+
+    scanf("%d", &T);
+
+    while(T--)
+    {
+        scanf("%s %s", str1, str2);
+
+        int str1_len = strlen(str1);
+        int str2_len = strlen(str2);
+
+        for(i=0; i<= (str1_len-str2_len); i++)
+        {
+            if(str2[0]==str1[i])
+            {
+                for(j=1; j<str2_len; j++)
+                {
+                    if(str2[j]!=str1[i+j])
+                        break;
+                }
+
+                if(j==str2_len)
+                {
+                    printf("%d\n", i);
+                    break;
+                }
+            }
+        }
+    }
+    return 0;
+}
